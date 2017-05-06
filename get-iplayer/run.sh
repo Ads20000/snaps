1436025ced35ed16809eff7d14f7556bb956ea75
Adam <adam1eveleigh@gmail.com>
2017-05-06 13:13:53 +0100

    Update run.sh

--- get-iplayer/run.sh
+++ get-iplayer/run.sh
@@ -3,7 +3,7 @@
 $SNAP/usr/bin/perl -I $SNAP/usr/share/perl/5.24 -I $SNAP/usr/lib/x86_64-linux-gnu/perl/5.24 -I $SNAP/usr/lib/x86_64-linux-gnu/perl5/5.24 -I $SNAP/usr/share/perl5 $SNAP/scripts/run.sh
 
 
-# Alternative run.sh
+# Alternative run.sh (less hardcoded, suggested by Perl IRC folks)
 # #!/bin/sh
 
 # OPTIONS=$(perl -e "print '-I$SNAP/',\$_,' ' for @INC")
@@ -11,7 +11,7 @@ $SNAP/usr/bin/perl -I $SNAP/usr/share/perl/5.24 -I $SNAP/usr/lib/x86_64-linux-gn
 # $SNAP/usr/bin/perl $OPTIONS $SNAP/scripts/get_iplayer
 
 
-# A perl run.sh (err, though maybe it should be called something else!? run.pl presumably?)
+# A perl run.sh (err, though maybe it should be called something else!? run.pl presumably?) (untested, suggested by Perl IRC folks)
 # #!/usr/bin/env perl
 
 # system("$ENV{SNAP}/usr/bin/perl", map({ "-I$ENV{SNAP}/$_" } @INC), "$ENV{SNAP}/scripts/get_iplayer");
